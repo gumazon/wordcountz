18a8a97b494860716ec98c8b3c0300aec193ad98
Gumshoe Media Inc <65322760+gumshoe00@users.noreply.github.com>
2022-01-28 00:03:10 -0700

    added plot bar chart or top N words of text in given file

--- src/wordcountz/__main__.py
+++ src/wordcountz/__main__.py
@@ -1,10 +1,60 @@
 #!/usr/bin/env python
 
 from pathlib import Path
+import matplotlib.pyplot as plt
 
 import sys
 
 from wordcountz import count_words
+
+
+class View:
+    @staticmethod
+    def plot_bar_top(n=10, word_counts=None, input_file=None):
+        """Plot a bar chart of
+
+        :param n: range or words to include in plot
+        :param word_counts: <Counter> word count of text in given file.
+        :return: bar chart of the most common n words.
+        """
+        if word_counts:
+            if input_file is None:
+                input_file = '{}/Public/wordcountz/temp/zen.txt'.format(Path.home())
+            input_file = Path(input_file)
+            _title = 'Top {} {} Words'.format(n, input_file.stem.capitalize())
+
+            top_n_words = word_counts.most_common(n)
+            word, count = zip(*top_n_words)
+
+            fig = plt.bar(range(n), count)
+            plt.title(_title)
+            plt.xticks(range(n), labels=word, rotation=30)
+            plt.ylabel('Word')
+            plt.xlabel('Count')
+            plt.savefig('{}/{}.png'.format(input_file.parent, '_'.join([_word.lower() for _word in _title.split(' ')])))
+            return fig
+
+
+class Controller:
+    def __init__(self, input_file=None):
+        self._input_file = input_file
+        self._model = self.__count_words(self.input_file)
+        self._view = View()
+
+    @property
+    def input_file(self):
+        return self._input_file
+
+    @input_file.setter
+    def input_file(self, path):
+        self._input_file = path
+
+    def plot_top(self, n=10):
+        wrds = self._model.words_count
+        print(wrds)
+        return self._view.plot_bar_top(n, wrds)
+
+    __count_words = count_words
 
 
 if __name__ == '__main__':
@@ -17,5 +67,6 @@ if __name__ == '__main__':
     else:
         _file = '{}/Public/wordcountz/temp/zen.txt'.format(Path.home())
 
-    print(count_words(_file))
+    _output = Controller(_file)
 
+    print(_output.plot_top())
--- tests/test_wordcountz.py
+++ tests/test_wordcountz.py
@@ -1,1 +1,14 @@
-from wordcountz import wordcountz
+from collections import Counter
+from pathlib import Path
+
+from wordcountz.wordcountz.src.wordcountz import count_words
+
+
+def test_count_words():
+    """Test word counting from a file."""
+
+    expected = Counter({'over': 2, 'and': 2, 'insanity': 1, 'is': 1, 'doing': 1, 'the': 1, 'same': 1, 'thing': 1, 'expecting': 1, 'different': 1, 'results': 1})
+
+    actual = count_words('{}/einstein.txt'.format(Path(__file__).parent))
+
+    assert actual == expected, "Einstein quote counted incorrectly!"
